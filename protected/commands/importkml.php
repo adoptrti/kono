f435c2c301de558292b6a365e29e09e5711c9f34
Vikas Yadav <mevikas@gmail.com>
2017-09-12 21:37:03 +0530

    wip

---
+++ protected/commands/importkml.php
@@ -0,0 +1,70 @@
+<?php
+$dom = new DOMDocument ();
+$file = realpath ( '../../docs/tn-maps/TAMILNADU-AC.kml' );
+if ($dom->load ( $file ) == false)
+    die ( "could not load file :$file" );
+echo "Loaded file $file\n";
+$xpath = new DOMXpath ( $dom );
+
+$ele = $dom->getElementsByTagName ( 'Placemark' );
+
+foreach ( $ele as $place )
+{
+    parseplace ( $place );
+}
+
+function parseplace($place)
+{
+    echo __LINE__ . "\n";
+    $ExtendedData = parseExtendedData ( $place );
+    $coords = parseCoords ( $place );
+    /*
+     * $nodes = $place->childNodes;
+     * foreach ($nodes as $node) {
+     * switch($node->nodeName)
+     * {
+     * case 'ExtendedData':
+     * $ExtendedData = parseExtendedData($node);
+     * break;
+     * case 'Polygon':
+     * $coords = parseCoords($node);
+     * break;
+     * }
+     * insertRow($ExtendedData,$coords);
+     * }
+     */
+    insertRow($ExtendedData,$coords);
+}
+
+function insertRow($ExtendedData, $coords)
+{
+    
+}
+
+function parseExtendedData($node)
+{
+    $rt = [ ];
+    $eles = $node->getElementsByTagName ( 'SimpleData' );
+    foreach ( $eles as $ele )
+    {
+        $rt [$ele->getAttribute ( 'name' )] = $ele->nodeValue;
+    }
+
+    return $rt;
+}
+
+function parseCoords($node)
+{
+    $eles = $node->getElementsByTagName ( 'coordinates' );
+    if ($eles->length == 0)
+        die ( "Not found coords\n" );
+    $ele = $eles->item ( 0 );
+    $c2 = array_map ( function ($item)
+    {
+        $a = explode ( ',', $item );
+        if (count ( $a ) == 3)
+            return $a;
+    }, explode ( ' ', $ele->nodeValue ) );
+    
+    return $c2;
+}
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -25,8 +25,27 @@
         geolocator.locate(options, function (err, location) {
             if (err) return console.log(err);
             console.log(location);
+            var frm = document.getElementById('floc');
+            
+            frm.elements['address'].value =JSON.stringify(location.address); 
+            frm.elements['coords'].value =JSON.stringify(location.coords);
+            <?php 
+            if(!isset($_POST['address'])) 
+            { 
+                ?>
+                frm.submit();
+                <?php 
+            } ?>                         
         });
     };
 
 </script>
+<?php if(isset($_POST['address']))
+    print_r(json_decode($_POST['address']));
+    print_r(json_decode($_POST['coords']));
+?> 
+<form method="post" action="/" id="floc">
+<input name="address" type="hidden"/>
+<input name="coords" type="hidden"/>
+</form>
 <div id="map-canvas" style="width:600px;height:400px"></div>
