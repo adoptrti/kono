e835ffd10099429c4ce5c77edbd1efa7cb6b10f1
Vikas Yadav <mevikas@gmail.com>
2017-09-13 23:41:12 +0530

    ward gets zone

--- protected/views/site/_ward.php
+++ protected/views/site/_ward.php
@@ -12,6 +12,14 @@
                     'data' => $data,
                     'attributes'=>array(
                             array(               // related city displayed as a link
+                                    'label'=> 'Zone',
+                                    'value' => function($data) {
+                                        $ward = AssemblyPolygon::model()->findByAttributes(['acno' => $data->wardno,'DT_CODE' => $data->DT_CODE,'ST_CODE' => $data->ST_CODE]);
+                                        if($ward)
+                                            return $ward->zone;
+                                    }
+                            ),
+                            array(               // related city displayed as a link
                                     'label'=> 'Elected Councillor Name',
                                     'name' => 'name',
                             ),
