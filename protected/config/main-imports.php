9fc5a7f8947ce07759e61147a9504bae6c8f77ee
Vikas Yadav <mevikas@gmail.com>
2017-11-17 00:56:30 -0800

    kono alias set

--- protected/config/main-imports.php
+++ protected/config/main-imports.php
@@ -1,6 +1,8 @@
 <?php
 #201512071925:vikas:#356:Coimbature
 #201709281811:thevikas:#8:Kovai
+Yii::setPathOfAlias('kono', __DIR__ . '/..');
+
 return array (
         'application.models.*',
         'application.commands.*',
