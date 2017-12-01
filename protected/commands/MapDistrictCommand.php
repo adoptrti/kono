0d08676a89608b787b14d05cd2abeebef2dda42d
Vikas Yadav <mevikas@gmail.com>
2017-12-01 21:20:09 +0530

    mapping district names with many other tables

--- protected/commands/MapDistrictCommand.php
+++ protected/commands/MapDistrictCommand.php
@@ -8,10 +8,12 @@ class MapDistrictCommand extends CConsoleCommand
 {
     var $state;
 
-    public function actionIndex()
+    public function actionpolygons()
     {
-        while($this->matchItem());
+        while ( $this->matchItem () )
+            ;
     }
+
     public function matchItem()
     {
         $id_state = 34;
@@ -22,21 +24,26 @@ where p.id_state=:sid and p.id_district is null and dist_name<>''" );
         $rs = $cmd->queryScalar ( [ 
                 'sid' => 34 
         ] );
-        if($rs===false)
+        if ($rs === false)
             return false;
-        $dist_name = $rs[0];
+        $dist_name = $rs [0];
         
-        $dists = District::model ()->bystate ( $id_state )->findAll (['order' => 'name']);
-        $dists2 = CHtml::listData ( $dists, 'id_district', function($data) {
-            if(count($data->polygons)==0)
-                return $data->name;
-            return false;
-        });
-        $disty_names = array_filter($dists2,function($item){
-           return !empty($item);
-        });
+        $dists = District::model ()->bystate ( $id_state )->findAll ( [ 
+                'order' => 'name' 
+        ] );
+        $dists2 = CHtml::listData ( $dists, 'id_district', 
+                function ($data)
+                {
+                    if (count ( $data->polygons ) == 0)
+                        return $data->name;
+                    return false;
+                } );
+        $disty_names = array_filter ( $dists2, function ($item)
+        {
+            return ! empty ( $item );
+        } );
         print_r ( $disty_names );
-            
+        
         echo "Match $dist_name?";
         $id = intval ( fgets ( STDIN ) );
         echo "Sure $dist_name is " . $dists2 [$id] . '?';
@@ -45,11 +52,69 @@ where p.id_state=:sid and p.id_district is null and dist_name<>''" );
             AssemblyPolygon::model ()->updateAll ( 
                     [ 
                             'id_district' => $id,
-                            'dist_name' => $dists2[$id] 
-                    ], 'id_state=? and dist_name=?', [ 
+                            'dist_name' => $dists2 [$id] 
+                    ], 'id_state=? and dist_name=?', 
+                    [ 
                             $id_state,
                             $dist_name 
                     ] );
+        return true;
+    }
+
+    public function actiontowns($id_state)
+    {
+        $this->state = State::model()->findbyPk($id_state);
+        while ( $this->matchItemTown( $id_state ) );
+    }
+
+    public function matchItemTown($id_state)
+    {
+        $cmd = Yii::app ()->db->createCommand ( 
+                "SELECT distinct dt_name FROM `towns2011`  where id_district is null and st_name<>dt_name and id_state=?" );
+        $rs = $cmd->queryScalar ( [ 
+                $id_state 
+        ] );
+        if ($rs === false)
+            return false;
+        $dist_name = $rs;
+        
+        $dists = District::model ()->bystate ( $id_state )->findAll ( [ 
+                'order' => 'name' 
+        ] );
+        $dists2 = CHtml::listData ( $dists, 'id_district', 
+                function ($data)
+                {
+                        return $data->name;
+                } );
+        $disty_names = array_filter ( $dists2, 
+                function ($item)
+                {
+                    return ! empty ( $item );
+                } );
+        
+        echo "Districts in {$this->state->name}\n";
+        print_r ( $disty_names );
+        
+        echo "Match $dist_name?";
+        $id = intval ( fgets ( STDIN ) );
+        echo "Sure $dist_name is " . $dists2 [$id] . '?';
+        $yn = trim ( fgets ( STDIN ) );
+        if ($yn == 'y')
+        {
+            Town::model ()->updateAll ( 
+                    [ 
+                            'id_state' => $id_state,
+                            'id_district' => $id,
+                            'dist_name' => $dists2 [$id] 
+                    ], 'id_state=? and dt_name=?', 
+                    [ 
+                            $id_state,
+                            $dist_name 
+                    ] );
+            $dist = District::model()->findByPk($id);
+            $dist->othername = $dist_name;
+            $dist->save();
+        }
         return true;
     }
 }
