aefa04cc119eec4a3940b7f55e593aa06fb222cb
Vikas Yadav <mevikas@gmail.com>
2018-05-13 18:22:54 +0530

    Credits page, first edition fixes #27

---
+++ credits.md
@@ -0,0 +1,7 @@
+# Credits
+
+## R-project
+For importing 100,000 row tables into csv
+
+## [Data{Meet}](http://datameet.org/)
+Most of the geolocation polygons were cleanup by the Datameet team and others. They have been given for use on [github](https://github.com/datameet).
---
+++ protected/views/site/pages/credits.php
@@ -0,0 +1,13 @@
+<?php
+/* @var $this SiteController */
+
+$this->pageTitle=Yii::app()->name . ' - ' . __('Credits');
+$this->breadcrumbs=array(
+        __('Credits'),
+);
+
+$text = file_get_contents(Yii::app()->basePath . '/../Credits.md');
+$md = new CMarkdown;
+echo $md->transform($text);
+
+?>
